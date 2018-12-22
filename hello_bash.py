cfb7e7fdc5dc9b75d5dfb071c67f9d35f5a17051
Tati <sholom.keller@gmail.com>
2018-12-21 22:14:50 -0500

    demonstrating use of 'echo' and redirection

--- hello_bash.py
+++ hello_bash.py
@@ -1,3 +1,4 @@
 print("I created the file using 'touch' and edited this line with nano")
 
 print("I reopened the file with nano and added another line")
+print('\n\nThis line was added to the file using the \'echo\' command and standard redirection')
